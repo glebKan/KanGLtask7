b77bc67671da6bd3a673b9e2a803b1b126365294
Kanareykin Gleb <gleb.canareykin2018@yandex.ru>
2022-04-09 14:22:52 +0300

    Добавьте файлы проекта.

---
+++ nomer7/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "krug.h"
+#include "vector.h"
+#include "complex.h"
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	Complex a(4,1);
+	Complex b(3,2);
+	a.sum(b);
+	a.print();
+}
